9a596dcb6d20ebf0328625b146dd058308c2e6f5
Junior Moura <natmourajr@gmail.com>
2016-02-19 21:55:32 -0200

    Creating MLP lib

---
+++ NeuralNetwork/MultiLayerPerceptron.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import timeit
+
+
+class MLP(object):
+    """Multi-Layer Perceptron Class """
+    
+    def __init__(self):
+    	print 'MLP Class Constructor'
---
+++ setup.sh
@@ -0,0 +1,8 @@
+echo
+echo
+echo "running Artificial Intelligence Setup Script"
+echo "any issue: natmourajr@gmail.com"
+echo
+echo
+
+export PYTHONPATH=$PYTHONPATH:$PWD
