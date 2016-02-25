ea0ba34ec3f6f838b62029f44cb71bd63c9150b0
Junior Moura <natmourajr@gmail.com>
2016-02-24 22:16:05 -0300

    Implement Train Function

--- NeuralNetwork/MultiLayerPerceptron.py
+++ NeuralNetwork/MultiLayerPerceptron.py
@@ -7,7 +7,6 @@ import numpy
 import theano
 import theano.tensor as T
 
-
 class InputLayer(object):
 	""" Input Layer Class """
 	
@@ -49,7 +48,11 @@ class InputLayer(object):
 	def GetOutput(self,input, show=False):
 		if show:
 			print "InputLayer: GetOutput Function"
-			print ""
+			
+		return (
+		input if self.pre_function is None
+		else input
+		)
 		
 					
 class HiddenLayer(object):
@@ -121,8 +124,9 @@ class HiddenLayer(object):
 			b = theano.shared(value=b_values, name='b', borrow=True)
 		self.b = b
 		
+		self.params = [self.W, self.b]
 		
-	
+		
 	def Show(self):
 		print ""
 		print "HiddenLayer Object"
@@ -131,6 +135,16 @@ class HiddenLayer(object):
 		print "Activation Function: ", self.activation
 		print "Initialization Function: ", "Random"
 		print ""
+
+	def GetOutput(self, input, show=False):
+		if show:
+			print "Hidden Layer: GetOuput Function"
+			
+		lin_output = T.dot(input, self.W) + self.b
+		return (
+            lin_output if self.activation is None
+            else self.activation(lin_output)
+        )
     	
 class OutputLayer(object):
 	""" Output Layer Class """
@@ -203,6 +217,8 @@ class OutputLayer(object):
 			b = theano.shared(value=b_values, name='b', borrow=True)
 		self.b = b
 		
+		self.params = [self.W, self.b]
+		
 	def Show(self):
 		print ""
 		print "OutputLayer Object"
@@ -212,10 +228,32 @@ class OutputLayer(object):
 		print "Initialization Function: ", "Random"
 		print ""
 		
+	def GetOutput(self, input, show=False):
+		if show:
+			print "Output Layer: GetOuput Function"
+		
+		lin_output = T.dot(input, self.W) + self.b
+		return (
+            lin_output if self.activation is None
+            else self.activation(lin_output)
+        )
+		
 		
 class TrainParameters(object):
-	def __init__(self,show=False):
+	def __init__(self,show=False, perf_function='mse', l1reg=0.0, l2reg=0.0001, n_epochs=10,learning_rate=0.01):
 		""" Train Parameters Class """
+		self.perf_function = perf_function
+		self.l1reg = l1reg
+		self.l2reg = l2reg
+		self.n_epochs = n_epochs
+		self.learning_rate = learning_rate
+
+	def Show(self):
+		print "Performance Function: ", self.perf_function
+		print "L1 Regularization Value: ", self.l1reg
+		print "L2 Regularization Value: ", self.l2reg
+		print "Number of Epochs: ", self.n_epochs
+		print "Learning Rate: ", self.learning_rate
 		
 		
 class MLP(object):
@@ -255,6 +293,10 @@ class MLP(object):
     	desc: Non linearity to be applied in the output layer
     	"""
     	
+    	self.n_in = n_in
+    	self.n_hidden = n_hidden
+    	self.n_out = n_out
+    	
     	self.InputLayer = InputLayer(n_in, n_in, pre_function=None, show=show)
     	
     	self.HiddenLayer = {}
@@ -266,6 +308,13 @@ class MLP(object):
     			self.HiddenLayer[i] = HiddenLayer(rng, n_in=n_hidden[i-1], n_out=n_hidden[i],show=show)
     			
     	self.OutputLayer = OutputLayer(rng,n_in=n_hidden[-1],n_out=n_out,show=show)
+    	
+    	params = []
+    	for i in range(len(n_hidden)):
+    		params = params + self.HiddenLayer[i].params
+    	params = params + self.OutputLayer.params
+    	
+    	self.params = params
 
     def Show(self):
     	""" This function show all relevant features of Neural Network Obj """
@@ -280,7 +329,43 @@ class MLP(object):
     		self.HiddenLayer[i].Show()
     	self.OutputLayer.Show()
     	
-    def Train(self, input=None, target=None, trn_params=None):
+    def GetOutput(self, input, show=False):
+    	if show:
+    		print "MLP: GetOutput Function"
+    		
+    	InputLayerOutput = self.InputLayer.GetOutput(input, show)
+    	
+    	HiddenLayerOutput = {}
+    	
+    	for i in range(len(self.n_hidden)):
+    		if i == 0:
+    			HiddenLayerOutput[i] = self.HiddenLayer[i].GetOutput(InputLayerOutput, show)
+    		else:
+    			HiddenLayerOutput[i] = self.HiddenLayer[i].GetOutput(HiddenLayerOutput[i-1], show)
+    	
+    	return self.OutputLayer.GetOutput(HiddenLayerOutput[len(self.n_hidden)-1],show)
+    
+    def GetL1Reg(self):
+    	l1_value = 0
+    	for i in range(len(self.n_hidden)):
+    		l1_value = abs(self.HiddenLayer[i].W).sum()
+    	return (l1_value + abs(self.OutputLayer.W).sum())
+    	
+    def GetL2Reg(self):
+    	l2_value = 0
+    	for i in range(len(self.n_hidden)):
+    		l2_value = (self.HiddenLayer[i].W **2 ).sum()
+    	return (l2_value + (self.OutputLayer.W **2 ).sum())
+
+    
+    def Performance(self, inputs, targets, perf_str, show=False):
+    	if show:
+    		print "Performance Function"
+    	if perf_str == "mse":
+    		return T.mean((targets-self.GetOutput(inputs)) ** 2)
+    		
+    	    	
+    def Train(self, inputs, targets, trn_params=None):
     	print "Train Function"
     	
     	"""
@@ -297,12 +382,49 @@ class MLP(object):
     	
     	"""
     	
-    	self.input = input
-    	self.target = target
-    	self.trn_params = trn_params
+    	self.inputs = inputs
+    	self.targets = targets
     	
-    	# propagate input in all layers
+    	if trn_params is None:
+    		self.trn_params = TrainParameters()
+    	else:
+    		self.trn_params = trn_params
     	
+    	 # allocate symbolic variables for the data
+    	index = T.lscalar()  # index to a [mini]batch
+    	sym_inputs  = T.matrix('sym_inputs')  # the data is presented as a symbolic variable
+    	sym_targets = T.matrix('sym_targets') # the target is presented as a symbolic variable
+    	
+    	
+    	cost = (
+    	self.Performance(sym_inputs, sym_targets,'mse')
+    	+ self.trn_params.l1reg*self.GetL1Reg() 
+    	+ self.trn_params.l2reg*self.GetL2Reg()
+    	)
+    	
+    	# compute the gradient of cost with respect to theta (sotred in params)
+    	# the resulting gradients will be stored in a list gparams
+    	gparams = [T.grad(cost, param) for param in self.params]
+    	# specify how to update the parameters of the model as a list of
+    	# (variable, update expression) pairs
+    	
+    	# given two lists of the same length, A = [a1, a2, a3, a4] and
+    	# B = [b1, b2, b3, b4], zip generates a list C of same size, where each
+    	# element is a pair formed from the two lists :
+    	#    C = [(a1, b1), (a2, b2), (a3, b3), (a4, b4)]
+    	
+    	# W' = W - learning_rate * gradient_cost
+    	# b' = b - learning_rate * gradient_cost
+    	
+    	updates = [
+    	(param, param - self.trn_params.learning_rate * gparam)
+    	for param, gparam in zip(self.params, gparams)
+    	]
+    	
+    	print "Starting Train Process with the follow parameters:"
+    	self.trn_params.Show()
+    	
+    	# Creating Train Model, Test Model and Validation Model
     	
     	
     	
